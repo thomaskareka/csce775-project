0af196d63aeec3b2bd24b87d91caadb4bef31e5c
Thomas Kareka <tkareka@email.sc.edu>
2026-03-24 21:03:07 -0400

    weighted screen scrolling reward

---
+++ game_env/rewards/screen_scroll amount.py
@@ -0,0 +1,15 @@
+import gymnasium as gym
+from . import register_reward
+
+@register_reward("screen_scroll_amount")
+class ScreenScrollReward(gym.Wrapper):
+    def __init__(self, env, weight = 0.1):
+        super().__init__(env)
+        self.value = weight
+
+    def step(self, action):
+        obs, reward, terminated, truncated, info = self.env.step(action)
+        scrolling = info.get('scrolling', 0)
+        r = (scrolling - 16) * self.value
+        print(r)
+        return obs, reward, terminated, truncated, info
