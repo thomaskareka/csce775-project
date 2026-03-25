1677c79f894754733f6fd233724ed5ff968fe8dd
Thomas Kareka <tkareka@email.sc.edu>
2026-03-24 22:20:29 -0400

    add death penalty using RAM observation (resets faster too), change death penalty to use life count

--- game_env/rewards/death_penalty.py
+++ game_env/rewards/death_penalty.py
@@ -6,9 +6,14 @@ class DeathPenalty(gym.Wrapper):
     def __init__(self, env, penalty = -1000):
         super().__init__(env)
         self.penalty = penalty
+        self.prev_lives = 0
     
     def step(self, action):
         obs, reward, terminated, truncated, info = self.env.step(action)
-        if terminated: reward += self.penalty
+        lives = info.get('lives', self.prev_lives)
+        if(lives < self.prev_lives):
+            reward += self.penalty
+            # print(f"death penalty applied: {reward}")
+        self.prev_lives = lives
 
         return obs, reward, terminated, truncated, info
---
+++ game_env/rewards/ram_death_penalty.py
@@ -0,0 +1,21 @@
+import gymnasium as gym
+from . import register_reward
+
+@register_reward("ram_death_penalty")
+class RamDeathPenalty(gym.Wrapper):
+    def __init__(self, env, penalty = -1000):
+        super().__init__(env)
+        self.penalty = penalty
+    
+    def step(self, action):
+        obs, reward, terminated, truncated, info = self.env.step(action)
+        ram = self.env.unwrapped.get_ram()
+        # player state, 0x06 is player dies, 0x0b is dying
+        is_dead = ram[0x000E] == 0x06 or ram[0x000E] == 0x0B
+        #vertical screen position, 1 = viewport, 0 = above, > 1 = below
+        is_below_screen = ram[0x00B5] > 0x01 and ram[0x000E] != 0x03 and ram[0x000E] != 0x07
+        terminated = terminated or is_dead or is_below_screen
+        if is_dead or is_below_screen:
+            reward += self.penalty
+
+        return obs, reward, terminated, truncated, info
